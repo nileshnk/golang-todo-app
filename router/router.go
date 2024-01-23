338a3de48ccf768cc27eb584520e14664e3fd485
Nilesh Kumar <nileshkumar@geekyants.com>
2024-01-23 11:45:14 +0530

    added signout handler

--- router/router.go
+++ router/router.go
@@ -11,11 +11,11 @@ import (
 func MainRouter(r chi.Router) {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		fs := http.FileServer(http.Dir("public"))
-		fs.ServeHTTP(w, r);
+		fs.ServeHTTP(w, r)
 	})
 
-	r.Route("/api", TodoApiRoutes);
-	r.Route("/auth", AuthApiRoutes);
+	r.Route("/api", TodoApiRoutes)
+	r.Route("/auth", AuthApiRoutes)
 }
 
 func TodoApiRoutes(r chi.Router) {
@@ -23,7 +23,7 @@ func TodoApiRoutes(r chi.Router) {
 	r.Get("/tasks", TodoController.GetAllTasks)
 
 	r.Post("/tasks", TodoController.AddTask)
-	
+
 	r.Patch("/tasks/{id}", TodoController.ChangeTaskStatus)
 
 	r.Put("/tasks/{id}", TodoController.EditTaskText)
@@ -34,4 +34,5 @@ func TodoApiRoutes(r chi.Router) {
 func AuthApiRoutes(r chi.Router) {
 	r.Post("/signup", AuthController.SignUp)
 	r.Post("/signin", AuthController.SignIn)
+	r.Get("/signout", AuthController.SignOut)
 }
