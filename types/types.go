5c3bcccf0fcd247e4638994e0b2d7a28abe1e41e
Nilesh Kumar <nileshkumar@geekyants.com>
2024-01-23 11:50:05 +0530

    Updated types. changed uuid type for user_id

--- types/types.go
+++ types/types.go
@@ -1,37 +1,45 @@
 package types
 
-import "database/sql"
+import (
+	"github.com/google/uuid"
+)
 
 type Task struct {
-	Id int `json:"id"`
-	Text string `json:"text"`
-	Completed bool `json:"completed"`
-	UserID sql.NullString `json:"user_id"`
+	Id        int64  `json:"id"`
+	Text      string `json:"text"`
+	Completed bool   `json:"completed"`
+	UserID    string `json:"user_id"`
 	CreatedAt string `json:"created_at"`
 	UpdatedAt string `json:"updated_at"`
 }
 
 type AppResponse struct {
-	Success bool `json:"success"`
-	Message string `json:"message"`
-	Data interface{} `json:"data"`
+	Success bool        `json:"success"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
 }
 
 type UserRegistration struct {
-	Id int `json:"id" `
-	Name string `json:"name" validate:"required"`
-	Email string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=6,max=30"`
-	PasswordConfirmation string `json:"password_confirmation" validate:"required,min=6,max=30"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
+	Name            string `json:"name" validate:"required"`
+	Email           string `json:"email" validate:"required,email"`
+	Password        string `json:"password" validate:"required,min=6,max=30"`
+	ConfirmPassword string `json:"confirm_password" validate:"required,min=6,max=30"`
 }
 
 type User struct {
-	Id int `json:"id"`
-	Name string `json:"name"`
-	Email string `json:"email"`
+	Id        uuid.UUID `json:"id"`
+	Name      string    `json:"name"`
+	Email     string    `json:"email"`
+	Password  string    `json:"password"`
+	CreatedAt string    `json:"created_at"`
+	UpdatedAt string    `json:"updated_at"`
+}
+
+type TokenPayload struct {
+	UserId uuid.UUID `json:"user_id"`
+}
+
+type SignInPayload struct {
+	Email    string `json:"email"`
 	Password string `json:"password"`
-	CreatedAt string `json:"created_at"`
-	UpdatedAt string `json:"updated_at"`
 }
